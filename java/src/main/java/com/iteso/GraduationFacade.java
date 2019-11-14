2d2c166c365f62bbc66e7f2768ccbff3f8cacace
Alejandro Rios <jassoalex97@hotmail.com>
2019-11-14 08:41:02 -0600

    Graduation facede inicializado

---
+++ java/src/main/java/com/iteso/GraduationFacade.java
@@ -0,0 +1,14 @@
+package com.iteso;
+
+
+public class GraduationFacade {
+    Graduate[] graduates;
+    Guest[] guests;
+    Waiter[] waiters:
+    Table[] tables;
+    Stage stage;
+    Food food;
+    Music music;
+    Light light;
+
+}
